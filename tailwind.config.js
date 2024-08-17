a712cfb177697aaac3f16e426812f8a62480e349
amitpanthi <apapanthi@gmail.com>
2024-08-18 00:46:26 +0530

    initial commit, added base page

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        background: '#fafafa',
+        primary: '#212121',
+        secondary: '#424242'
+      }
+    },
+  },
+  plugins: [],
+}
+
